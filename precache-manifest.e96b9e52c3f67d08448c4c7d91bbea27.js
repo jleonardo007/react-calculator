34a1378dc0ad38d66df4935d0a798b1765a310b9
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2020-03-06 14:14:13 -0400

    Updates

---
+++ precache-manifest.e96b9e52c3f67d08448c4c7d91bbea27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4564c3923c0f55ebe53eb3011dfcac4",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "c8b8fb6332a23d73a37e",
+    "url": "/react-calculator/static/css/main.db65456c.chunk.css"
+  },
+  {
+    "revision": "fa82d1f4c2c453fc9908",
+    "url": "/react-calculator/static/js/2.b9844c1b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-calculator/static/js/2.b9844c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b8fb6332a23d73a37e",
+    "url": "/react-calculator/static/js/main.26f4d0ff.chunk.js"
+  },
+  {
+    "revision": "6b779269f3314e1e1a31",
+    "url": "/react-calculator/static/js/runtime-main.2f2d5c98.js"
+  }
+]);
